56427096ca5a6ba84791776fba5ef4e84b38ae0e
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-21 15:43:54 -0600

    debug

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 :: Temporary fix for missing libgfortran.dll.a - remove when r-base is fixed
-dir %PREFIX%\lib\R\etc
-sed -i 's/FLIBS = .*$/FLIBS = /' %PREFIX%\lib\R\etc\Makeconf
+sed -i 's/FLIBS = .*$/FLIBS = /' %PREFIX%\lib\R\etc\x64\Makeconf
 IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 "%R%" CMD INSTALL --build . %R_ARGS%
