8b2d06a43193176fcd26e3f9737153499d52f1e2
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-20 20:40:02 -0800

    try temporary fix for Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 :: Temporary fix for missing libgfortran.dll.a - remove when r-base is fixed
-sed -i 's/FLIBS = .*$/FLIBS = /'
+sed -i 's/FLIBS = .*$/FLIBS = /' %LIBRARY_PREFIX%\lib\R\etc\Makeconf
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
